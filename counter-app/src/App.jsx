bad74ef9b301e5f108b781a33ecd09cacd8f1f34
VilayatKhan <mohdvilayatali.khan@arup.com>
2024-03-20 20:31:08 +0530

    Define a custom component to use under parent component

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <button
-      onClick={() => {
-        setCount(count + 1);
-      }}
-    >
-      Counter : {count}
-    </button>
+    <div>
+      <CustomButton count={count} setCount={setCount} />
+    </div>
   );
 }
 
+function CustomButton(props) {
+  const onClickHandler = () => {
+    props.setCount(props.count + 1);
+  };
+  return <button onClick={onClickHandler}>Counter: {props.count}</button>;
+}
+
+CustomButton.propTypes = {
+  count: PropTypes.number.isRequired,
+  setCount: PropTypes.func.isRequired,
+};
 export default App;
